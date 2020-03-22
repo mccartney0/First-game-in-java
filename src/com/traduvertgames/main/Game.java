0069430b6faa2837e9ae4e76d65b144a6dcc4eea
Allan Gomes <traduvert@icloud.com>
2020-03-22 11:25:29 -0300

    Created - Items and Map

---
+++ src/com/traduvertgames/main/Game.java
@@ -0,0 +1,130 @@
+package com.traduvertgames.main;
+
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+import javax.swing.JFrame;
+
+public class Game extends Canvas implements Runnable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static JFrame frame;
+	private Thread thread;
+	private boolean isRunning = true;
+	private final int WIDTH = 240;
+	private final int HEIGHT = 160;
+	private final int SCALE = 3;
+
+	private BufferedImage image;
+
+
+	public Game() throws IOException {
+		setPreferredSize(new Dimension(WIDTH * SCALE, HEIGHT * SCALE));
+		initFrame();
+		image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_BGR);
+	}
+
+	public void initFrame() {
+		frame = new JFrame("Firt Gamer");
+		frame.add(this);
+		frame.setResizable(false);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+	}
+
+	public synchronized void start() {
+		thread = new Thread(this);
+		isRunning = true;
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		isRunning = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		Game game = new Game();
+		game.start();
+	}
+
+	public void update() {
+
+	}
+
+	public void render() { // Renderização funciona por ordem de código, primeira linhas, segunda, etc...
+
+		BufferStrategy bs = this.getBufferStrategy();
+		if (bs == null) {
+			this.createBufferStrategy(3);
+			return;
+		}
+		Graphics g = image.getGraphics();
+		g.setColor(new Color(0, 0, 0)); //Cor de fundo
+		g.fillRect(0, 0, WIDTH, HEIGHT);
+
+//		Renderizar jogo
+
+//		Graphics2D g2 = (Graphics2D) g;
+
+//		g2.setColor(new Color(0,0,0,120));  // Escurecendo RGBS
+//		g2.fillRect(0, 0, WIDTH, HEIGHT);
+		
+//		
+		g.dispose();
+
+		g = bs.getDrawGraphics();
+		g.drawImage(image, 0, 0, WIDTH*SCALE, HEIGHT*SCALE, null);
+		bs.show();
+	}
+
+	@Override
+	public void run() {
+
+		long lastTime = System.nanoTime();
+		double amountOfUpdates = 60.0;
+		double ns = 1000000000 / amountOfUpdates;
+		double delta = 0;
+		int frames = 0;
+		double timer = System.currentTimeMillis();
+
+		while (isRunning) {
+
+			long now = System.nanoTime();
+			delta += (now - lastTime) / ns;
+			lastTime = now;
+
+			if (delta >= 1) {
+				update();
+				render();
+				frames++;
+				delta--;
+			}
+
+			if (System.currentTimeMillis() - timer >= 1000) {
+				System.out.println("FPS: " + frames);
+				frames = 0;
+				timer += 1000;
+			}
+
+		}
+
+		stop();
+	}
+
+}
