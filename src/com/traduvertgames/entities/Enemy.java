8287ecef0eb0395cb9351a469349849e91b4c2d0
Allan Gomes <traduvert@icloud.com>
2020-03-24 21:14:15 -0300

    Enemy Animation

--- src/com/traduvertgames/entities/Enemy.java
+++ src/com/traduvertgames/entities/Enemy.java
@@ -1,6 +1,6 @@
 package com.traduvertgames.entities;
 
-import java.awt.Color;
+
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
@@ -11,43 +11,54 @@ import com.traduvertgames.world.World;
 
 public class Enemy extends Entity {
 
-	private double speed = 0.6;
-	//Máscara do inimigo
-	private int maskx=8,masky=8,maskw=5,maskh=5;
+	private double speed = 0.5;
+	private int frames = 0, maxFrames = 20, index = 0, maxIndex = 1;
+	private BufferedImage[] sprites;
+//	private int maskx=8,masky=8,maskw=10,maskh=10;
 
 	public Enemy(int x, int y, int width, int height, BufferedImage sprite) {
-		super(x, y, width, height, sprite);
-
+		super(x, y, width, height, null);
+		sprites = new BufferedImage[2];
+		sprites[0] = Game.spritesheet.getSprite(112,16,16,16);
+		sprites[1] = Game.spritesheet.getSprite(112+16,16,16,16);
 	}
 
 	public void update() {
 //		if (Game.rand.nextInt(100) < 50) { // Fazendo os NPCs não se colidirem 1#
 		// IA dos mobs seguindo o jogador
 		if ((int) x < Game.player.getX() && World.isFree((int) (x + speed), this.getY()) 
-				&& !isColiding((int) (x + speed), this.getY())) {
+				&& !isColliding((int) (x + speed), this.getY())) {
 			x += speed;
 		} else if ((int) x > Game.player.getX() && World.isFree((int) (x - speed), this.getY())
-				&& !isColiding((int) (x - speed), this.getY())) {
+				&& !isColliding((int) (x - speed), this.getY())) {
 			x -= speed;
 		}
 		if ((int) y < Game.player.getY() && World.isFree(this.getX(), (int) (y + speed))
-				&& !isColiding(this.getX(), (int) (y + speed))) {
+				&& !isColliding(this.getX(), (int) (y + speed))) {
 			y += speed;
 		} else if ((int) y > Game.player.getY() && World.isFree(this.getX(), (int) (y - speed))
-				&& !isColiding(this.getX(), (int) (y - speed))) {
+				&& !isColliding(this.getX(), (int) (y - speed))) {
 			y -= speed;
 //			}
 		}
+			frames++;
+			if (frames == maxFrames) {
+				frames = 0;
+				index++;
+				if (index > maxIndex) {
+					index = 0;
+				}
+			}
 	}
 
 	// Fazer nenhum NPC colidir com o outro
-	public boolean isColiding(int xNext, int yNext) {
-		Rectangle enemyCurrent = new Rectangle(xNext+maskx, yNext+maskw,maskw,maskh);
+	public boolean isColliding(int xNext, int yNext) {
+		Rectangle enemyCurrent = new Rectangle(xNext, yNext,World.TILE_SIZE,World.TILE_SIZE);
 		for (int i = 0; i < Game.enemies.size(); i++) {
 			Enemy e = Game.enemies.get(i);
 			if (e == this)
 				continue;
-			Rectangle targetEnemy = new Rectangle(e.getX()+maskx,e.getY()+maskw,maskw,maskh);
+			Rectangle targetEnemy = new Rectangle(e.getX(),e.getY(),World.TILE_SIZE,World.TILE_SIZE);
 			if(enemyCurrent.intersects(targetEnemy)) {
 				return true;
 			}
@@ -57,9 +68,14 @@ public class Enemy extends Entity {
 	}
 	
 	//Trocar a máscara quadrada para nao colidir
-	public void render (Graphics g) {
-		super.render(g);
-		g.setColor(Color.blue);
-		g.fillRect(this.getX()+maskx-Camera.x,this.getY()+ masky-Camera.y,maskw,maskh);
+//	public void render (Graphics g) {
+//		super.render(g);
+//		g.setColor(Color.blue);
+//		g.fillRect(this.getX()+maskx-Camera.x,this.getY()+ masky-Camera.y,maskw,masky);
+//	}
+	
+	public void render(Graphics g) {
+		// * Sempre que renderizar, tem que por a posição da Camera
+		g.drawImage(sprites[index], this.getX()-Camera.x, this.getY()-Camera.y,null);
 	}
 }
--- src/com/traduvertgames/entities/Player.java
+++ src/com/traduvertgames/entities/Player.java
@@ -14,20 +14,20 @@ public class Player extends Entity {
 	public int dir = right_dir;
 	public double speed = 1.5;
 
-	private int frames = 0, maxFrames = 5, index = 0, maxIndex = 3;
+	private int frames = 0, maxFrames = 7, index = 0, maxIndex = 3;
 	private boolean moved = false;
 	private BufferedImage[] rightPlayer;
 	private BufferedImage[] leftPlayer;
-//	private BufferedImage[] upPlayer;
-//	private BufferedImage[] downPlayer;
+	private BufferedImage[] upPlayer;
+	private BufferedImage[] downPlayer;
 
 	public Player(int x, int y, int width, int height, BufferedImage sprite) {
 		super(x, y, width, height, sprite);
 
 		rightPlayer = new BufferedImage[4];
 		leftPlayer = new BufferedImage[4];
-//		upPlayer = new BufferedImage[4];
-//		downPlayer = new BufferedImage[4];
+		upPlayer = new BufferedImage[4];
+		downPlayer = new BufferedImage[4];
 
 		for (int i = 0; i < 4; i++) {
 			rightPlayer[i] = Game.spritesheet.getSprite(32 + (i * 16), 0, 16, 16);
@@ -41,9 +41,15 @@ public class Player extends Entity {
 //		for (int i = 1; i < 4; i++) {
 //			downPlayer[i] = Game.spritesheet.getSprite(64 + (i * 16), 32, 16, 16);
 //		}
-//		for (int i = 1; i < 4; i=1) {
-//			downPlayer[i] = Game.spritesheet.getSprite(8*16, 16, 16, 16);
-//		}
+		upPlayer[0] = Game.spritesheet.getSprite(32,32,16,16);
+		upPlayer[1] = Game.spritesheet.getSprite(32+16,32,16,16);
+		upPlayer[2] = Game.spritesheet.getSprite(32,32,16,16);
+		upPlayer[3] = Game.spritesheet.getSprite(32+16,32,16,16);
+		
+		downPlayer[0] = Game.spritesheet.getSprite(64,32,16,16);
+		downPlayer[1] = Game.spritesheet.getSprite(64+16,32,16,16);
+		downPlayer[2] = Game.spritesheet.getSprite(64,32,16,16);
+		downPlayer[3] = Game.spritesheet.getSprite(64+16,32,16,16);
 	}
 
 	public void update() {
@@ -61,16 +67,15 @@ public class Player extends Entity {
 		}
 		if (up && World.isFree(this.getX(),(int)(y-speed))) {
 			moved = true;
-//			dir = up_dir;
+			dir = up_dir;
 			y -= speed;
 		} else if (down && World.isFree(this.getX(),(int)(y+speed))) {
-//			dir = down_dir;
+			dir = down_dir;
 			moved = true;
 			y += speed;
 		}
 		if (moved) {
 			frames++;
-			System.out.println(frames);
 			if (frames == maxFrames) {
 				frames = 0;
 				index++;
@@ -94,10 +99,12 @@ public class Player extends Entity {
 		} else if (dir == left_dir) {
 			g.drawImage(leftPlayer[index], this.getX() - Camera.x, this.getY() - Camera.y, null);
 		}
-//		if (dir == up_dir) {
-//			g.drawImage(upPlayer[index], this.getX(), this.getY(), null);
-//		 if (dir == down_dir) {
-//			g.drawImage(downPlayer[index], this.getX(), this.getY(), null);
-//		}
+		if (dir == up_dir) {
+			g.drawImage(upPlayer[index], this.getX()-Camera.x, this.getY()-Camera.y,null);
+		}
+		 if (dir == down_dir) {
+			g.drawImage(downPlayer[index], this.getX()-Camera.x, this.getY()-Camera.y,null);
+			 
+		}
 	}
 }
